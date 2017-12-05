e111ed8414e7c0f60fa79537000a3ebbdc2b13d1
HarryStevens <harryjosephstevens@gmail.com>
2017-12-05 15:40:45 +0530

    Update GJ 2017 with correct districts and sub regions

--- meta_data/constituency_lookup/Gujarat2017.js
+++ meta_data/constituency_lookup/Gujarat2017.js
@@ -3,623 +3,623 @@ module.exports = [
    "constituency": "ABDASA",
    "ac_no": 1,
    "reservations": "GEN",
-   "district": "KACHCHH",
-   "sub_region": "KACHCHH"
+   "district": "Kachchh",
+   "sub_region": "Kutch"
  },
  {
    "constituency": "ANJAR",
    "ac_no": 4,
    "reservations": "GEN",
-   "district": "KACHCHH",
-   "sub_region": "KACHCHH"
+   "district": "Kachchh",
+   "sub_region": "Kutch"
  },
  {
    "constituency": "ANKLESHWAR",
    "ac_no": 154,
    "reservations": "GEN",
-   "district": "TAPI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Bharuch",
+   "sub_region": "Central Gujarat"
  },
  {
    "constituency": "BARDOLI (SC)",
    "ac_no": 169,
    "reservations": "SC",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "BHARUCH",
    "ac_no": 153,
    "reservations": "GEN",
-   "district": "TAPI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Bharuch",
+   "sub_region": "Central Gujarat"
  },
  {
    "constituency": "BHAVNAGAR EAST",
    "ac_no": 104,
    "reservations": "GEN",
-   "district": "PANCHMAHAL",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "BHAVNAGAR RURAL",
    "ac_no": 103,
    "reservations": "GEN",
-   "district": "MAHISAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "BHAVNAGAR WEST",
    "ac_no": 105,
    "reservations": "GEN",
-   "district": "PANCHMAHAL",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "BHUJ",
    "ac_no": 3,
    "reservations": "GEN",
-   "district": "KACHCHH",
-   "sub_region": "KACHCHH"
+   "district": "Kachchh",
+   "sub_region": "Kutch"
  },
  {
    "constituency": "BOTAD",
    "ac_no": 107,
    "reservations": "GEN",
-   "district": "DAHOD",
-   "sub_region": "SAURASHTRA"
+   "district": "Botad",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "CHORYASI",
    "ac_no": 168,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "CHOTILA",
    "ac_no": 63,
    "reservations": "GEN",
-   "district": "JAMNAGAR",
-   "sub_region": "AHMEDABAD RURAL"
+   "district": "Surendranagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "DANGS (ST)",
    "ac_no": 173,
    "reservations": "ST",
-   "district": "DANG",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Dangs",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "DASADA (SC)",
    "ac_no": 60,
    "reservations": "SC",
-   "district": "JAMNAGAR",
-   "sub_region": "AHMEDABAD RURAL"
+   "district": "Surendranagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "DHARAMPUR (ST)",
    "ac_no": 178,
    "reservations": "ST",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Valsad",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "DHARI",
    "ac_no": 94,
    "reservations": "GEN",
-   "district": "ANAND",
-   "sub_region": "SAURASHTRA"
+   "district": "Amreli",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "DHORAJI",
    "ac_no": 75,
    "reservations": "GEN",
-   "district": "JUNAGADH",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "DHRANGADHRA",
    "ac_no": 64,
    "reservations": "GEN",
-   "district": "JAMNAGAR",
-   "sub_region": "AHMEDABAD RURAL"
+   "district": "Surendranagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "DWARKA",
    "ac_no": 82,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Devbhumi Dwarka",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "GADHADA (SC)",
    "ac_no": 106,
    "reservations": "SC",
-   "district": "PANCHMAHAL",
-   "sub_region": "SAURASHTRA"
+   "district": "Botad",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "GANDHIDHAM (SC)",
    "ac_no": 5,
    "reservations": "SC",
-   "district": "KACHCHH",
-   "sub_region": "KACHCHH"
+   "district": "Kachchh",
+   "sub_region": "Kutch"
  },
  {
    "constituency": "GARIADHAR",
    "ac_no": 101,
    "reservations": "GEN",
-   "district": "KHEDA",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "GONDAL",
    "ac_no": 73,
    "reservations": "GEN",
-   "district": "JUNAGADH",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JAMJODHPUR",
    "ac_no": 80,
    "reservations": "GEN",
-   "district": "AMRELI",
-   "sub_region": "SAURASHTRA"
+   "district": "Jamnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JAMNAGAR NORTH",
    "ac_no": 78,
    "reservations": "GEN",
-   "district": "AMRELI",
-   "sub_region": "SAURASHTRA"
+   "district": "Jamnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JAMNAGAR RURAL",
    "ac_no": 77,
    "reservations": "GEN",
-   "district": "SOMNATH",
-   "sub_region": "SAURASHTRA"
+   "district": "Jamnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JAMNAGAR SOUTH",
    "ac_no": 79,
    "reservations": "GEN",
-   "district": "AMRELI",
-   "sub_region": "SAURASHTRA"
+   "district": "Jamnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JASDAN",
    "ac_no": 72,
    "reservations": "GEN",
-   "district": "JUNAGADH",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JETPUR",
    "ac_no": 74,
    "reservations": "GEN",
-   "district": "JUNAGADH",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "JUNAGADH",
    "ac_no": 86,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Junagarh",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "KALAVAD (SC)",
    "ac_no": 76,
    "reservations": "SC",
-   "district": "SOMNATH",
-   "sub_region": "SAURASHTRA"
+   "district": "Jamnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "KAMREJ",
    "ac_no": 158,
    "reservations": "GEN",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "KARANJ",
-   "ac_no": 147,
+   "ac_no": 162,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "KATARGAM",
    "ac_no": 166,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "KESHOD",
    "ac_no": 88,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Junagarh",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "KHAMBHALIA",
    "ac_no": 81,
    "reservations": "GEN",
-   "district": "AMRELI",
-   "sub_region": "SAURASHTRA"
+   "district": "Devbhumi Dwarka",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "KODINAR (SC)",
    "ac_no": 92,
    "reservations": "SC",
-   "district": "ANAND",
-   "sub_region": "SAURASHTRA"
+   "district": "Gir Somnath",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "KUTIYANA",
    "ac_no": 84,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Porbandar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "LATHI",
    "ac_no": 96,
    "reservations": "GEN",
-   "district": "KHEDA",
-   "sub_region": "SAURASHTRA"
+   "district": "Amreli",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "LIMBAYAT",
    "ac_no": 163,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "LIMBDI",
    "ac_no": 61,
    "reservations": "GEN",
-   "district": "JAMNAGAR",
-   "sub_region": "AHMEDABAD RURAL"
+   "district": "Surendranagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "MAHUVA",
    "ac_no": 99,
    "reservations": "GEN",
-   "district": "KHEDA",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "MANAVADAR",
    "ac_no": 85,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Junagarh",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "MANDVI",
    "ac_no": 2,
    "reservations": "GEN",
-   "district": "KACHCHH",
-   "sub_region": "KACHCHH"
+   "district": "Kachchh",
+   "sub_region": "Kutch"
  },
  {
    "constituency": "MANDVI (ST)",
    "ac_no": 157,
    "reservations": "ST",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "MANGROL",
    "ac_no": 89,
    "reservations": "GEN",
-   "district": "BOTAD",
-   "sub_region": "SAURASHTRA"
+   "district": "Junagarh",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "MORBI",
    "ac_no": 65,
    "reservations": "GEN",
-   "district": "JAMNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Morbi",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "NIZAR (ST)",
    "ac_no": 172,
    "reservations": "ST",
-   "district": "TAPI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Tapi",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "OLPAD",
    "ac_no": 155,
    "reservations": "GEN",
-   "district": "DANG",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "PALITANA",
    "ac_no": 102,
    "reservations": "GEN",
-   "district": "MAHISAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "PARDI",
    "ac_no": 180,
    "reservations": "GEN",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Valsad",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "PORBANDAR",
    "ac_no": 83,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Porbandar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "RAJKOT EAST",
    "ac_no": 68,
    "reservations": "GEN",
-   "district": "DEVBHOOMI DWARKA",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "RAJKOT RURAL (SC)",
    "ac_no": 71,
    "reservations": "SC",
-   "district": "PORBANDAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "RAJKOT SOUTH",
    "ac_no": 70,
    "reservations": "GEN",
-   "district": "PORBANDAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "RAJKOT WEST",
    "ac_no": 69,
    "reservations": "GEN",
-   "district": "PORBANDAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Rajkot",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "RAJULA",
    "ac_no": 98,
    "reservations": "GEN",
-   "district": "KHEDA",
-   "sub_region": "SAURASHTRA"
+   "district": "Amreli",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "RAPAR",
    "ac_no": 6,
    "reservations": "GEN",
-   "district": "KACHCHH",
-   "sub_region": "KACHCHH"
+   "district": "Kachchh",
+   "sub_region": "Kutch"
  },
  {
    "constituency": "SAVARKUNDLA",
    "ac_no": 97,
    "reservations": "GEN",
-   "district": "KHEDA",
-   "sub_region": "SAURASHTRA"
+   "district": "Amreli",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "SOMNATH",
    "ac_no": 90,
    "reservations": "GEN",
-   "district": "BOTAD",
-   "sub_region": "SAURASHTRA"
+   "district": "Gir Somnath",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "SURAT EAST",
    "ac_no": 159,
    "reservations": "GEN",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "SURAT NORTH",
    "ac_no": 160,
    "reservations": "GEN",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "TALAJA",
    "ac_no": 100,
    "reservations": "GEN",
-   "district": "KHEDA",
-   "sub_region": "SAURASHTRA"
+   "district": "Bhavnagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "TALALA",
    "ac_no": 91,
    "reservations": "GEN",
-   "district": "ANAND",
-   "sub_region": "SAURASHTRA"
+   "district": "Gir Somnath",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "TANKARA",
    "ac_no": 66,
    "reservations": "GEN",
-   "district": "DEVBHOOMI DWARKA",
-   "sub_region": "SAURASHTRA"
+   "district": "Morbi",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "UDHNA",
    "ac_no": 164,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "UMBERGAON (ST)",
    "ac_no": 182,
    "reservations": "ST",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Valsad",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "VAGRA",
    "ac_no": 151,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Bharuch",
+   "sub_region": "Central Gujarat"
  },
  {
    "constituency": "VARACHHA ROAD",
    "ac_no": 161,
    "reservations": "GEN",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "VISAVADAR",
    "ac_no": 87,
    "reservations": "GEN",
-   "district": "BHAVNAGAR",
-   "sub_region": "SAURASHTRA"
+   "district": "Junagarh",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "WADHWAN",
    "ac_no": 62,
    "reservations": "GEN",
-   "district": "JAMNAGAR",
-   "sub_region": "AHMEDABAD RURAL"
+   "district": "Surendranagar",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "WANKANER",
    "ac_no": 67,
    "reservations": "GEN",
-   "district": "DEVBHOOMI DWARKA",
-   "sub_region": "SAURASHTRA"
+   "district": "Morbi",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "AMRELI",
    "ac_no": 95,
    "reservations": "GEN",
-   "district": "ANAND",
-   "sub_region": "SAURASHTRA"
+   "district": "Amreli",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "BANSDA (ST)",
    "ac_no": 177,
    "reservations": "ST",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Navsari",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "DEDIAPADA (ST)",
    "ac_no": 149,
    "reservations": "ST",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Narmada",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "GANDEVI (ST)",
    "ac_no": 176,
    "reservations": "ST",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Navsari",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "JALALPORE",
    "ac_no": 174,
    "reservations": "GEN",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Navsari",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "JAMBUSAR",
    "ac_no": 150,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Bharuch",
+   "sub_region": "Central Gujarat"
  },
  {
    "constituency": "JHAGADIA (ST)",
    "ac_no": 152,
    "reservations": "ST",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Bharuch",
+   "sub_region": "Central Gujarat"
  },
  {
    "constituency": "KAPRADA (ST)",
    "ac_no": 181,
    "reservations": "ST",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Valsad",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "MAHUVA (ST)",
    "ac_no": 170,
    "reservations": "ST",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "MAJURA",
    "ac_no": 165,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "MANGROL (ST)",
    "ac_no": 156,
    "reservations": "ST",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "NANDOD (ST)",
    "ac_no": 148,
    "reservations": "ST",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Narmada",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "NAVSARI",
    "ac_no": 175,
    "reservations": "GEN",
-   "district": "NAVSARI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Navsari",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "SURAT WEST",
    "ac_no": 167,
    "reservations": "GEN",
-   "district": "SURAT",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Surat",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "UNA",
    "ac_no": 93,
    "reservations": "GEN",
-   "district": "ANAND",
-   "sub_region": "SAURASHTRA"
+   "district": "Gir Somnath",
+   "sub_region": "Saurashtra"
  },
  {
    "constituency": "VALSAD",
    "ac_no": 179,
    "reservations": "GEN",
-   "district": "VALSAD",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Valsad",
+   "sub_region": "South Gujarat"
  },
  {
    "constituency": "VYARA (ST)",
    "ac_no": 171,
    "reservations": "ST",
-   "district": "TAPI",
-   "sub_region": "SOUTH GUJARAT"
+   "district": "Tapi",
+   "sub_region": "South Gujarat"
  }
 ]
