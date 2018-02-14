55890bdf6797c069ffabf3742cf413daef7ddf64
HarryStevens <harryjosephstevens@gmail.com>
2018-02-14 14:31:24 +0530

    Add Tripura 2013

--- meta_data/elections.js
+++ meta_data/elections.js
@@ -3,5 +3,6 @@ module.exports = [
 	{state: "Himachal Pradesh", year: "2012", id: "hp2012", table_index: 2},
 	{state: "Gujarat", year: "2012", id: "gujarat2012", table_index: 2},
 	{state: "Gujarat", year: "2017", id: "Gujarat2017", table_index: 2},
-	{state: "Tripura", year: "2018", id: "tripura2018", table_index: 1}
+	{state: "Tripura", year: "2018", id: "tripura2018", table_index: 1},
+	{state: "Tripura", year: "2013", id: "tripura2013", table_index: 2}
 ]
