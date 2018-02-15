b57ef6565d753c413f79cb9d98cc248e6ba95a97
HarryStevens <harryjosephstevens@gmail.com>
2018-02-15 12:28:08 +0530

    Spaces

--- scrapers/candidate_details.js
+++ scrapers/candidate_details.js
@@ -1,9 +1,9 @@
 var io = require("indian-ocean"),
-	_ = require("underscore"),
-	select = require("../helpers/select_election.js"),
-	request = require("request"),
-	cheerio = require("cheerio"),
-	jz = require("jeezy");
+  _ = require("underscore"),
+  select = require("../helpers/select_election.js"),
+  request = require("request"),
+  cheerio = require("cheerio"),
+  jz = require("jeezy");
 
 _.rateLimit = function(func, rate, async) {
   var queue = [];
@@ -38,117 +38,117 @@ var data = [];
 select.select(cb);
 
 function cb(object){
-	var state = object.state,
-		year = object.year,
-		id = object.id;
+  var state = object.state,
+    year = object.year,
+    id = object.id;
 
-	console.log("Scraping candidate details from the " + year + " " + state + " election.");
+  console.log("Scraping candidate details from the " + year + " " + state + " election.");
 
-	request("http://myneta.info/" + id + "/index.php?action=summary&subAction=candidates_analyzed&sort=candidate", function(error, response, body){
-		if (!error && response.statusCode == 200){
+  request("http://myneta.info/" + id + "/index.php?action=summary&subAction=candidates_analyzed&sort=candidate", function(error, response, body){
+    if (!error && response.statusCode == 200){
 
-			var $ = cheerio.load(body);
+      var $ = cheerio.load(body);
 
-			$("table").each(function(table_index, table){
+      $("table").each(function(table_index, table){
 
-				if (table_index == object.table_index){
+        if (table_index == object.table_index){
 
-					var rows = $(table).find("tr");
+          var rows = $(table).find("tr");
 
-					console.log("Rows: ", rows.length);
+          console.log("Rows: ", rows.length);
 
-					var scrape_row_limited = _.rateLimit(scrape_row, 1000);
+          var scrape_row_limited = _.rateLimit(scrape_row, 1000);
 
-					rows.each(function(row_index, row){
+          rows.each(function(row_index, row){
 
-						if (row_index > 1){
-							scrape_row_limited(row_index, row);
-						}
+            if (row_index > 1){
+              scrape_row_limited(row_index, row);
+            }
 
-					});
+          });
 
-					function scrape_row(row_index, row){
+          function scrape_row(row_index, row){
 
-						var obj = {};
-						obj.state = state;
-						obj.year = year;
+            var obj = {};
+            obj.state = state;
+            obj.year = year;
 
-						var cells = $(row).find("td");
+            var cells = $(row).find("td");
 
-						cells.each(function(cell_index, cell){
+            cells.each(function(cell_index, cell){
 
-							var cell_text = $(cell).text().trim();
+              var cell_text = $(cell).text().trim();
 
-							if (cell_index == 1){
-								obj.candidate_name = cell_text;
-								obj.url = "http://myneta.info/" + id + "/" + $(cell).find("a").attr("href");
-							} else if (cell_index == 2){
-								obj.constituency = cell_text;
-							} else if (cell_index == 3){
-								obj.party = cell_text;
-							}
+              if (cell_index == 1){
+                obj.candidate_name = cell_text;
+                obj.url = "http://myneta.info/" + id + "/" + $(cell).find("a").attr("href");
+              } else if (cell_index == 2){
+                obj.constituency = cell_text;
+              } else if (cell_index == 3){
+                obj.party = cell_text;
+              }
 
-						});
+            });
 
-						request(obj.url, function(error, response, body){
+            request(obj.url, function(error, response, body){
 
-							if (!error && response.statusCode == 200){
-								
-								var $ = cheerio.load(body);
+              if (!error && response.statusCode == 200){
+                
+                var $ = cheerio.load(body);
 
-								// get if they won
-								var name_text = $(".main-title").text().trim();
-								obj.winner = name_text.indexOf("(Winner)") != -1 ? true: false;
+                // get if they won
+                var name_text = $(".main-title").text().trim();
+                obj.winner = name_text.indexOf("(Winner)") != -1 ? true: false;
 
-								// more info
-								var columns0 = ["party", "so_do", "age", "address", "email", "phone"]
-								$(".grid_2.alpha").each(function(i, d){
-									
-									if (i !== 0){									
-										obj[columns0[i]] = $(d).text().split(":")[1].trim();
-	
-										// age is a number
-										if (i == 2){
-											obj[columns0[i]] = +obj[columns0[i]];
-										}
-									}
+                // more info
+                var columns0 = ["party", "so_do", "age", "address", "email", "phone"]
+                $(".grid_2.alpha").each(function(i, d){
+                  
+                  if (i !== 0){                  
+                    obj[columns0[i]] = $(d).text().split(":")[1].trim();
+  
+                    // age is a number
+                    if (i == 2){
+                      obj[columns0[i]] = +obj[columns0[i]];
+                    }
+                  }
 
-								});
+                });
 
-								// profession
-								$("p").each(function(i, d){
-									if (i == 0){
-										var profession_split = $(d).html().split(":");
-										
-										obj.profession = profession_split[1] ? profession_split[1].split("<br")[0].split(">")[1] : "";
-										obj.spouse_profession = profession_split[2] ? profession_split[2].replace(/\n/g, "").replace("</b>", "") : "";
-									}
+                // profession
+                $("p").each(function(i, d){
+                  if (i == 0){
+                    var profession_split = $(d).html().split(":");
+                    
+                    obj.profession = profession_split[1] ? profession_split[1].split("<br")[0].split(">")[1] : "";
+                    obj.spouse_profession = profession_split[2] ? profession_split[2].replace(/\n/g, "").replace("</b>", "") : "";
+                  }
 
-								});
+                });
 
-								var pct = (row_index + 1) / rows.length * 100;
-								console.log(pct.toFixed(2) + "%");
+                var pct = (row_index + 1) / rows.length * 100;
+                console.log(pct.toFixed(2) + "%");
 
-								data.push(obj);
+                data.push(obj);
 
-								// don't write till the end
-								if (pct > 90) io.writeDataSync("data/" + jz.str.toSlugCase(state) + "/" + year + "/" + jz.str.toSlugCase(state) + "_" + year + "_candidate-details.csv", data);	
-								
-							} else {
-								console.log("Error scraping " + obj.url);
-							}
+                // don't write till the end
+                if (pct > 90) io.writeDataSync("data/" + jz.str.toSlugCase(state) + "/" + year + "/" + jz.str.toSlugCase(state) + "_" + year + "_candidate-details.csv", data);  
+                
+              } else {
+                console.log("Error scraping " + obj.url);
+              }
 
-						});
+            });
 
-					}
-					
-				}
+          }
+          
+        }
 
-			});
+      });
 
-		} else {
-			console.log("Error in request", error);
-		}
-	});
+    } else {
+      console.log("Error in request", error);
+    }
+  });
 
 }
--- scrapers/index.js
+++ scrapers/index.js
@@ -1,21 +1,21 @@
 var select = require("../helpers/select_election"),
-	candidates = require("../helpers/get_candidates"),
-	itr = require("../helpers/get_itr"),
-	no_pan = require("../helpers/get_no_pan"),
-	serious_cases = require("../helpers/get_serious_cases"),
-	women = require("../helpers/get_women"),
-	recontest = require("../helpers/get_recontest");
+  candidates = require("../helpers/get_candidates"),
+  itr = require("../helpers/get_itr"),
+  no_pan = require("../helpers/get_no_pan"),
+  serious_cases = require("../helpers/get_serious_cases"),
+  women = require("../helpers/get_women"),
+  recontest = require("../helpers/get_recontest");
 
 select.select(obj => {
-	candidates.go(obj, obj => {
-		itr.go(obj, obj => {
-			no_pan.go(obj, obj => {
-				serious_cases.go(obj, obj => {
-					women.go(obj, obj => {
-						recontest.go(obj);
-					});
-				});
-			});
-		});
-	});
+  candidates.go(obj, obj => {
+    itr.go(obj, obj => {
+      no_pan.go(obj, obj => {
+        serious_cases.go(obj, obj => {
+          women.go(obj, obj => {
+            recontest.go(obj);
+          });
+        });
+      });
+    });
+  });
 });
