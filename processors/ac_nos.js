2db0d5bfe5c92631d2b30412ffdf9121218c7fce
HarryStevens <harryjosephstevens@gmail.com>
2018-02-20 15:14:10 +0530

    Add error log if constituency is missing from lookup table

--- processors/ac_nos.js
+++ processors/ac_nos.js
@@ -73,7 +73,7 @@ select.select(obj => {
 				m = lookup.filter(f => f.constituency == d.constituency)[0];	
 			}
 			if (m == undefined) {
-				console.log(d.constituency + " constituency is missing from your lookup table! Add it and try again.");
+				console.log("ERROR: " + d.constituency + " constituency is missing from your lookup table! Add it and try again.");
 				process.exit();
 			}
 			cols.forEach(c => {
