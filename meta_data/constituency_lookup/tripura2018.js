129fc7c6cdd0d20a877317c953f29ca6002d2400
HarryStevens <harryjosephstevens@gmail.com>
2018-03-07 12:32:19 +0530

    Fix tripura 2018 constituency lookup

--- meta_data/constituency_lookup/tripura2018.js
+++ meta_data/constituency_lookup/tripura2018.js
@@ -1,422 +1,422 @@
 module.exports = [
-  {
-    "constituency": "SIMNA",
-    "ac_no": 1,
-    "reservations": "ST",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "MOHANPUR",
-    "ac_no": 2,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BAMUTIA",
-    "ac_no": 3,
-    "reservations": "SC",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BARJALA",
-    "ac_no": 4,
-    "reservations": "SC",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "KHAYERPUR",
-    "ac_no": 5,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "AGARTALA",
-    "ac_no": 6,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "RAMNAGAR",
-    "ac_no": 7,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "TOWN BARDOWALI",
-    "ac_no": 8,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BANAMALIPUR",
-    "ac_no": 9,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "MAJLISHPUR",
-    "ac_no": 10,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "MANDAI BAZAR",
-    "ac_no": 11,
-    "reservations": "ST",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "TAKARJALA",
-    "ac_no": 12,
-    "reservations": "ST",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "PRATAPGARH",
-    "ac_no": 13,
-    "reservations": "SC",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BADHARGHAT",
-    "ac_no": 14,
-    "reservations": "SC",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "KAMALASAGAR",
-    "ac_no": 15,
-    "reservations": "GEN",
-    "district": "West Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BISHALGARH",
-    "ac_no": 16,
-    "reservations": "GEN",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "GOLAGHATI",
-    "ac_no": 17,
-    "reservations": "ST",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "SURYAMANINAGAR",
-    "ac_no": 18,
-    "reservations": "ST",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "CHARILAM",
-    "ac_no": 19,
-    "reservations": "ST",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BOXANAGAR",
-    "ac_no": 20,
-    "reservations": "GEN",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "NALCHAR",
-    "ac_no": 21,
-    "reservations": "SC",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "SONAMURA",
-    "ac_no": 22,
-    "reservations": "GEN",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "DHANPUR",
-    "ac_no": 23,
-    "reservations": "GEN",
-    "district": "Sipahijala",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "RAMCHANDRAGHAT",
-    "ac_no": 24,
-    "reservations": "ST",
-    "district": "Khowai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KHOWAI",
-    "ac_no": 25,
-    "reservations": "GEN",
-    "district": "Khowai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "ASHARAMBARI",
-    "ac_no": 26,
-    "reservations": "ST",
-    "district": "Khowai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KALYANPUR-PRAMODNAGAR",
-    "ac_no": 27,
-    "reservations": "GEN",
-    "district": "Khowai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "TELIAMURA",
-    "ac_no": 28,
-    "reservations": "ST",
-    "district": "Khowai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KRISHNAPUR",
-    "ac_no": 29,
-    "reservations": "ST",
-    "district": "Khowai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "BAGMA",
-    "ac_no": 30,
-    "reservations": "ST",
-    "district": "South Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "RADHAKISHORPUR",
-    "ac_no": 31,
-    "reservations": "ST",
-    "district": "South Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "MATARBARI",
-    "ac_no": 32,
-    "reservations": "ST",
-    "district": "Gomati",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "KAKRABAN-SHALGARA",
-    "ac_no": 33,
-    "reservations": "SC",
-    "district": "Gomati",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "RAJNAGAR",
-    "ac_no": 34,
-    "reservations": "SC",
-    "district": "South Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "BELONIA",
-    "ac_no": 35,
-    "reservations": "GEN",
-    "district": "South Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "SANTIRBAZAR",
-    "ac_no": 36,
-    "reservations": "ST",
-    "district": "South Tripura",
-    "sub_region": "Tripura West"
-  },
-  {
-    "constituency": "HRISHYAMUKH",
-    "ac_no": 37,
-    "reservations": "GEN",
-    "district": "South Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "JOLAIBARI",
-    "ac_no": 38,
-    "reservations": "ST",
-    "district": "South Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "MANU",
-    "ac_no": 39,
-    "reservations": "ST",
-    "district": "South Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "SABROOM",
-    "ac_no": 40,
-    "reservations": "GEN",
-    "district": "South Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "AMPINAGAR",
-    "ac_no": 41,
-    "reservations": "ST",
-    "district": "Gomati",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "AMARPUR",
-    "ac_no": 42,
-    "reservations": "GEN",
-    "district": "Gomati",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KARBOOK",
-    "ac_no": 43,
-    "reservations": "ST",
-    "district": "Gomati",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "RAIMA VALLEY",
-    "ac_no": 44,
-    "reservations": "ST",
-    "district": "Dhalai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KAMALPUR",
-    "ac_no": 45,
-    "reservations": "GEN",
-    "district": "Dhalai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "SURMA",
-    "ac_no": 46,
-    "reservations": "SC",
-    "district": "Dhalai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "AMBASSA",
-    "ac_no": 47,
-    "reservations": "ST",
-    "district": "Dhalai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KARAMCHARA",
-    "ac_no": 48,
-    "reservations": "ST",
-    "district": "Dhalai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "CHAWMANU",
-    "ac_no": 49,
-    "reservations": "ST",
-    "district": "Dhalai",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "PABIACHARA",
-    "ac_no": 50,
-    "reservations": "SC",
-    "district": "Unakoti",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "FATIKROY",
-    "ac_no": 51,
-    "reservations": "SC",
-    "district": "Unakoti",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "CHANDIPUR",
-    "ac_no": 52,
-    "reservations": "GEN",
-    "district": "Unakoti",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KAILASHAHAR",
-    "ac_no": 53,
-    "reservations": "GEN",
-    "district": "Unakoti",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KADAMTALA-KURTI",
-    "ac_no": 54,
-    "reservations": "GEN",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "BAGBASSA",
-    "ac_no": 55,
-    "reservations": "GEN",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "DHARMANAGAR",
-    "ac_no": 56,
-    "reservations": "GEN",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "JUBARAJNAGAR",
-    "ac_no": 57,
-    "reservations": "GEN",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "PENCHARTHAL",
-    "ac_no": 59,
-    "reservations": "ST",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "KANCHANPUR",
-    "ac_no": 60,
-    "reservations": "ST",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  },
-  {
-    "constituency": "PANISAGAR",
-    "ac_no": 58,
-    "reservations": "GEN",
-    "district": "North Tripura",
-    "sub_region": "Tripura East"
-  }
+ {
+   "constituency": "SIMNA",
+   "ac_no": 1,
+   "reservations": "ST",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "MOHANPUR",
+   "ac_no": 2,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BAMUTIA",
+   "ac_no": 3,
+   "reservations": "SC",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BARJALA",
+   "ac_no": 4,
+   "reservations": "SC",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "KHAYERPUR",
+   "ac_no": 5,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "AGARTALA",
+   "ac_no": 6,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "RAMNAGAR",
+   "ac_no": 7,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "TOWN BARDOWALI",
+   "ac_no": 8,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BANAMALIPUR",
+   "ac_no": 9,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "MAJLISHPUR",
+   "ac_no": 10,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "MANDAI BAZAR",
+   "ac_no": 11,
+   "reservations": "ST",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "TAKARJALA",
+   "ac_no": 12,
+   "reservations": "ST",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "PRATAPGARH",
+   "ac_no": 13,
+   "reservations": "SC",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BADHARGHAT",
+   "ac_no": 14,
+   "reservations": "SC",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "KAMALASAGAR",
+   "ac_no": 15,
+   "reservations": "GEN",
+   "district": "West Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BISHALGARH",
+   "ac_no": 16,
+   "reservations": "GEN",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "GOLAGHATI",
+   "ac_no": 17,
+   "reservations": "ST",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "SURYAMANINAGAR",
+   "ac_no": 18,
+   "reservations": "GEN",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "CHARILAM",
+   "ac_no": 19,
+   "reservations": "ST",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BOXANAGAR",
+   "ac_no": 20,
+   "reservations": "GEN",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "NALCHAR",
+   "ac_no": 21,
+   "reservations": "SC",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "SONAMURA",
+   "ac_no": 22,
+   "reservations": "GEN",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "DHANPUR",
+   "ac_no": 23,
+   "reservations": "GEN",
+   "district": "Sipahijala",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "RAMCHANDRAGHAT",
+   "ac_no": 24,
+   "reservations": "ST",
+   "district": "Khowai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KHOWAI",
+   "ac_no": 25,
+   "reservations": "GEN",
+   "district": "Khowai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "ASHARAMBARI",
+   "ac_no": 26,
+   "reservations": "ST",
+   "district": "Khowai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KALYANPUR-PRAMODNAGAR",
+   "ac_no": 27,
+   "reservations": "GEN",
+   "district": "Khowai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "TELIAMURA",
+   "ac_no": 28,
+   "reservations": "GEN",
+   "district": "Khowai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KRISHNAPUR",
+   "ac_no": 29,
+   "reservations": "ST",
+   "district": "Khowai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "BAGMA",
+   "ac_no": 30,
+   "reservations": "ST",
+   "district": "South Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "RADHAKISHORPUR",
+   "ac_no": 31,
+   "reservations": "GEN",
+   "district": "South Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "MATARBARI",
+   "ac_no": 32,
+   "reservations": "GEN",
+   "district": "Gomati",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "KAKRABAN-SHALGARA",
+   "ac_no": 33,
+   "reservations": "SC",
+   "district": "Gomati",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "RAJNAGAR",
+   "ac_no": 34,
+   "reservations": "SC",
+   "district": "South Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "BELONIA",
+   "ac_no": 35,
+   "reservations": "GEN",
+   "district": "South Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "SANTIRBAZAR",
+   "ac_no": 36,
+   "reservations": "ST",
+   "district": "South Tripura",
+   "sub_region": "Tripura West"
+ },
+ {
+   "constituency": "HRISHYAMUKH",
+   "ac_no": 37,
+   "reservations": "GEN",
+   "district": "South Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "JOLAIBARI",
+   "ac_no": 38,
+   "reservations": "ST",
+   "district": "South Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "MANU",
+   "ac_no": 39,
+   "reservations": "ST",
+   "district": "South Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "SABROOM",
+   "ac_no": 40,
+   "reservations": "GEN",
+   "district": "South Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "AMPINAGAR",
+   "ac_no": 41,
+   "reservations": "ST",
+   "district": "Gomati",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "AMARPUR",
+   "ac_no": 42,
+   "reservations": "GEN",
+   "district": "Gomati",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KARBOOK",
+   "ac_no": 43,
+   "reservations": "ST",
+   "district": "Gomati",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "RAIMA VALLEY",
+   "ac_no": 44,
+   "reservations": "ST",
+   "district": "Dhalai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KAMALPUR",
+   "ac_no": 45,
+   "reservations": "GEN",
+   "district": "Dhalai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "SURMA",
+   "ac_no": 46,
+   "reservations": "SC",
+   "district": "Dhalai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "AMBASSA",
+   "ac_no": 47,
+   "reservations": "ST",
+   "district": "Dhalai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KARAMCHARA",
+   "ac_no": 48,
+   "reservations": "ST",
+   "district": "Dhalai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "CHAWMANU",
+   "ac_no": 49,
+   "reservations": "ST",
+   "district": "Dhalai",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "PABIACHARA",
+   "ac_no": 50,
+   "reservations": "SC",
+   "district": "Unakoti",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "FATIKROY",
+   "ac_no": 51,
+   "reservations": "SC",
+   "district": "Unakoti",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "CHANDIPUR",
+   "ac_no": 52,
+   "reservations": "GEN",
+   "district": "Unakoti",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KAILASHAHAR",
+   "ac_no": 53,
+   "reservations": "GEN",
+   "district": "Unakoti",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KADAMTALA-KURTI",
+   "ac_no": 54,
+   "reservations": "GEN",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "BAGBASSA",
+   "ac_no": 55,
+   "reservations": "GEN",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "DHARMANAGAR",
+   "ac_no": 56,
+   "reservations": "GEN",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "JUBARAJNAGAR",
+   "ac_no": 57,
+   "reservations": "GEN",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "PENCHARTHAL",
+   "ac_no": 59,
+   "reservations": "ST",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "KANCHANPUR",
+   "ac_no": 60,
+   "reservations": "ST",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ },
+ {
+   "constituency": "PANISAGAR",
+   "ac_no": 58,
+   "reservations": "GEN",
+   "district": "North Tripura",
+   "sub_region": "Tripura East"
+ }
 ]
