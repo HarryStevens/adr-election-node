27418f799f091599b8761af01094a93699f87264
Harry Stevens <harryjosephstevens@gmail.com>
2018-11-25 12:34:49 -0500

    Add Mizoram 2018

---
+++ meta_data/constituency_lookup/mizoram2018.js
@@ -0,0 +1,282 @@
+module.exports = [
+  {
+    "constituency": "AIZAWL EAST I",
+    "ac_no": 13,
+    "reservations": "GEN",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL EAST II",
+    "ac_no": 14,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL NORTH I",
+    "ac_no": 10,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL NORTH II",
+    "ac_no": 11,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL NORTH III",
+    "ac_no": 12,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL SOUTH I",
+    "ac_no": 18,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL SOUTH II",
+    "ac_no": 19,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL SOUTH III",
+    "ac_no": 20,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL WEST I",
+    "ac_no": 15,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL WEST II",
+    "ac_no": 16,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "AIZAWL WEST III",
+    "ac_no": 17,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "CHALFILH",
+    "ac_no": 8,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "TAWI",
+    "ac_no": 9,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "TUIVAWL",
+    "ac_no": 7,
+    "reservations": "ST",
+    "district": "AIZAWL",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "CHAMPHAI NORTH",
+    "ac_no": 23,
+    "reservations": "ST",
+    "district": "CHAMPHAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "CHAMPHAI SOUTH",
+    "ac_no": 24,
+    "reservations": "ST",
+    "district": "CHAMPHAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "EAST TUIPUI",
+    "ac_no": 25,
+    "reservations": "ST",
+    "district": "CHAMPHAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LENGTENG",
+    "ac_no": 21,
+    "reservations": "ST",
+    "district": "CHAMPHAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "TUICHANG",
+    "ac_no": 22,
+    "reservations": "ST",
+    "district": "CHAMPHAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "KOLASIB",
+    "ac_no": 5,
+    "reservations": "ST",
+    "district": "KOLASIB",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "SERLUI",
+    "ac_no": 6,
+    "reservations": "ST",
+    "district": "KOLASIB",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "TUIRIAL",
+    "ac_no": 4,
+    "reservations": "ST",
+    "district": "KOLASIB",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LAWNGTLAI EAST",
+    "ac_no": 38,
+    "reservations": "ST",
+    "district": "LAWNGTLAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LAWNGTLAI WEST",
+    "ac_no": 37,
+    "reservations": "ST",
+    "district": "LAWNGTLAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "TUICHAWNG",
+    "ac_no": 36,
+    "reservations": "ST",
+    "district": "LAWNGTLAI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LUNGLEI EAST",
+    "ac_no": 31,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LUNGLEI NORTH",
+    "ac_no": 30,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LUNGLEI SOUTH",
+    "ac_no": 33,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "LUNGLEI WEST",
+    "ac_no": 32,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "SOUTH TUIPUI",
+    "ac_no": 29,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "THORANG",
+    "ac_no": 34,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "WEST TUIPUI",
+    "ac_no": 35,
+    "reservations": "ST",
+    "district": "LUNGLEI",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "DAMPA",
+    "ac_no": 2,
+    "reservations": "ST",
+    "district": "MAMIT",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "HACHHEK",
+    "ac_no": 1,
+    "reservations": "ST",
+    "district": "MAMIT",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "MAMIT",
+    "ac_no": 3,
+    "reservations": "ST",
+    "district": "MAMIT",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "PALAK",
+    "ac_no": 40,
+    "reservations": "ST",
+    "district": "SAIHA",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "SAIHA",
+    "ac_no": 39,
+    "reservations": "ST",
+    "district": "SAIHA",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "HRANGTURZO",
+    "ac_no": 28,
+    "reservations": "ST",
+    "district": "SERCHHIP",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "SERCHHIP",
+    "ac_no": 26,
+    "reservations": "ST",
+    "district": "SERCHHIP",
+    "sub_region": "NA"
+  },
+  {
+    "constituency": "TUIKUM",
+    "ac_no": 27,
+    "reservations": "ST",
+    "district": "SERCHHIP",
+    "sub_region": "NA"
+  }
+]
