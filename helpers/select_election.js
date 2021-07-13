ca5a0189b448141c0df9d7eaa658d38c52d78d0c
HarryStevens <harryjosephstevens@gmail.com>
2021-07-13 18:09:24 -0400

    Replace tabs with spaces

--- helpers/select_election.js
+++ helpers/select_election.js
@@ -8,37 +8,37 @@ const elections = require("../meta_data/elections");
 
 // callback returns obj with state, year, and id
 module.exports.select = function(callback){
-	const states = unique(elections, "state");	
-	
-	inquirer
-	  .prompt([
+  const states = unique(elections, "state");  
+  
+  inquirer
+    .prompt([
       {
         type: "search-list",
         message: "Select a state",
         name: "state",
         choices: states
       }
-	  ])
-	  .then(({state}) => {
-			const years = unique(_.where(elections, {state, state}), "year");
+    ])
+    .then(({state}) => {
+      const years = unique(_.where(elections, {state, state}), "year");
 
-			console.log(`You entered ${chalk.blue.bold(state)}. Which election year?`);
+      console.log(`You entered ${chalk.blue.bold(state)}. Which election year?`);
 
-			cliSelect({
-				values: years,
-				valueRenderer: (value, selected) => selected ? chalk.underline(value) : value
-			})
-			.then(res => {
-				const year = res.value;
-				const obj = _.where(elections, {state: state, year: year})[0];
-				
-				callback(obj);
-			});
+      cliSelect({
+        values: years,
+        valueRenderer: (value, selected) => selected ? chalk.underline(value) : value
+      })
+      .then(res => {
+        const year = res.value;
+        const obj = _.where(elections, {state: state, year: year})[0];
+        
+        callback(obj);
+      });
 
-		});
+    });
 
 }
 
 function unique(data, variable){
-	return _.chain(data).pluck(variable).uniq().value();
+  return _.chain(data).pluck(variable).uniq().value();
 }
